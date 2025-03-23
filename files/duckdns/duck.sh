b66ebd4a4f8ed58901370144d6a6cdbf063509b2
Ryan McClean <${USER_EMAIL}>
2025-03-23 22:59:51 +0000

    Updates to crontab

--- files/duckdns/duck.sh
+++ files/duckdns/duck.sh
@@ -1,1 +1,14 @@
-echo url="https://www.duckdns.org/update?domains=${PLEX_URL},${HASS_URL},${BASE_URL}&token=${DUCK_TOKEN}&ip=" | curl -k -o ./duck.log -K -
+#!/bin/bash
+. /home/${USER}/dotfiles/.env
+
+echo "Updating DuckDNS" > ./duck.log
+
+for URL in "${DUCK_URLS[@]}"; do
+  echo url="https://www.duckdns.org/update?domains=${URL}&token=${DUCK_TOKEN}&ip=" | curl -k -K - &>/dev/null
+  if [ $? -eq 0 ]; then
+    echo "DuckDNS: ${URL} updated" >> ./duck.log
+  else
+    echo "DuckDNS: ${URL} update failed" >> ./duck.log
+  fi
+done
+
