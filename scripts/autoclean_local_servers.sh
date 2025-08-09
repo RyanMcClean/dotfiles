a35e950477b6d392a5369325d0bf66167fca464a
Ryan McClean <${USER_EMAIL}>
2025-08-09 14:57:36 +0100

    Enhance server management scripts: add autoclean functionality, improve SSH error handling, and update settings for cSpell

--- scripts/autoclean_local_servers.sh
+++ scripts/autoclean_local_servers.sh
@@ -13,19 +13,20 @@ for server in ${LOCAL_SERVERS[@]}; do
     if [[ -z "${server}" ]]; then
         continue
     elif [[ "${server}" == $(hostname) ]]; then
-        echo "Upgrading self: ${server}"
-        sudo apt-get update && sudo apt-get upgrade -y
+        echo "Cleaning self: ${server}"
+        sudo apt-get autoclean && sudo apt-get autoremove -y && sudo apt-get clean
     elif ! ssh -o PasswordAuthentication=no "$server" /bin/true 2> /dev/null; then
         echo "Attempted to connect to: $server"
         echo "SSH connection failed when using keys, not trying with password authentication, please copy keys to server and try again."
-        echo
+        echo 
         echo
         continue
     else 
-        echo "Updating server: $server"
-        echo "SSH connection successful, updating server now"
-        ssh "$server" "sudo apt-get update && sudo apt-get upgrade -y"
+        echo "Cleaning server: $server"
+        echo "SSH connection successful, cleaning server now"
+        ssh "$server" "sudo apt-get autoclean && sudo apt-get autoremove -y && sudo apt-get clean"
     fi
     echo
     echo
 done
+
