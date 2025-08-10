21cd55fb28b3e0b5c60643dd815255417bd21d2d
Ryan McClean <${USER_EMAIL}>
2025-08-10 18:40:24 +0100

    Update DuckDNS script and configuration; enhance crontab setup for yt-dlp

--- files/duckdns/duck.sh
+++ files/duckdns/duck.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 . /home/${USER}/dotfiles/.env
 
-echo "Updating DuckDNS" > ./duck.log
+duck_log="/home/${USER}/.duckdns/duck.log"
+
+echo "Updating DuckDNS" > "${duck_log}"
 
 for URL in "${DUCK_URLS[@]}"; do
   echo url="https://www.duckdns.org/update?domains=${URL}&token=${DUCK_TOKEN}&ip=" | curl -k -K - &>/dev/null
   if [ $? -eq 0 ]; then
-    echo "DuckDNS: ${URL} updated" >> ./duck.log
+    echo "DuckDNS: ${URL} updated" >> "${duck_log}"
   else
-    echo "DuckDNS: ${URL} update failed" >> ./duck.log
+    echo "DuckDNS: ${URL} update failed" >> "${duck_log}"
   fi
 done
 
